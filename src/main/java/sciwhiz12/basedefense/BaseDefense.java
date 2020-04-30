d174ae633a0654ba934884fff9e56d3ba4efc33c
Arnold Alejo Nunag <arnoldnunag12@gmail.com>
2020-04-30 21:10:42 +0800

    Rename base mod class

---
+++ src/main/java/sciwhiz12/basedefense/BaseDefense.java
@@ -0,0 +1,16 @@
+package sciwhiz12.basedefense;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BaseDefense.MODID)
+public class BaseDefense {
+    public static final String MODID = "basedefense";
+    
+    public static final Logger LOG = LogManager.getLogger(MODID);
+
+    public BaseDefense() {
+    }
+}
--- src/main/java/sciwhiz12/basedefense/BaseDefenseMod.java
+++
@@ -1,8 +0,0 @@
-package sciwhiz12.basedefense;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(BaseDefenseMod.MODID)
-public class BaseDefenseMod {
-    public static final String MODID = "basedefense";
-}
