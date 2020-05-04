68b872f21412da9b7ae0d5d422dcdeec2e8d8580
Arnold Alejo Nunag <arnoldnunag12@gmail.com>
2020-05-04 10:55:01 +0800

    Add colors to Keys
    Currently only accessible through manual NBT modification

--- src/main/java/sciwhiz12/basedefense/BaseDefense.java
+++ src/main/java/sciwhiz12/basedefense/BaseDefense.java
@@ -3,12 +3,14 @@ package sciwhiz12.basedefense;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import sciwhiz12.basedefense.client.color.IdentifyingColor;
 import sciwhiz12.basedefense.client.gui.KeysmithScreen;
 import sciwhiz12.basedefense.client.gui.LocksmithScreen;
 
@@ -32,5 +34,6 @@ public class BaseDefense {
         BDBlocks.setupRenderLayer();
         ScreenManager.registerFactory(BDBlocks.KEYSMITH_CONTAINER.get(), KeysmithScreen::new);
         ScreenManager.registerFactory(BDBlocks.LOCKSMITH_CONTAINER.get(), LocksmithScreen::new);
+        Minecraft.getInstance().getItemColors().register(new IdentifyingColor(), BDItems.KEY.get());
     }
 }
---
+++ src/main/java/sciwhiz12/basedefense/client/color/IdentifyingColor.java
@@ -0,0 +1,25 @@
+package sciwhiz12.basedefense.client.color;
+
+import net.minecraft.client.renderer.color.IItemColor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.IntArrayNBT;
+
+public class IdentifyingColor implements IItemColor {
+    @Override
+    public int getColor(ItemStack stack, int tintIndex) {
+        if (tintIndex > 0) {
+            CompoundNBT tag = stack.getChildTag("display");
+            if (tag != null && tag.contains("colors")) {
+                int[] colors = tag.getIntArray("colors");
+                if (colors.length > 0 && tintIndex == 1) {
+                    return colors[0];
+                } else if (colors.length > 1 && tintIndex == 2) {
+                    return colors[1];
+                }
+            }
+            return 0xffd5c000;
+        }
+        return 0xFFFFFFFF;
+    }
+}
