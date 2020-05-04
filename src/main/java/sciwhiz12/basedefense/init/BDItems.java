ca9ebf0d92fba6bee0ed5e89d73ace0db029b8ba
Arnold Alejo Nunag <arnoldnunag12@gmail.com>
2020-05-04 11:34:34 +0800

    Add item group

--- src/main/java/sciwhiz12/basedefense/init/BDItems.java
+++ src/main/java/sciwhiz12/basedefense/init/BDItems.java
@@ -1,7 +1,11 @@
 package sciwhiz12.basedefense.init;
 
+import javax.annotation.Nonnull;
+
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -10,36 +14,41 @@ import sciwhiz12.basedefense.item.lock.KeyItem;
 import sciwhiz12.basedefense.item.lock.LockItem;
 
 public class BDItems {
+    public static final ItemGroup GROUP = new ItemGroup(BaseDefense.MODID) {
+        @Override
+        @Nonnull
+        public ItemStack createIcon() {
+            return new ItemStack(BDItems.LOCK.get());
+        }
+    };
     public static final DeferredRegister<Item> ITEM = new DeferredRegister<>(
             ForgeRegistries.ITEMS, BaseDefense.MODID
     );
 
     public static final RegistryObject<Item> BLANK_KEY = ITEM.register(
-            "blank_key", () -> new Item(new Item.Properties())
+            "blank_key", () -> new Item(new Item.Properties().group(GROUP))
     );
-
     public static final RegistryObject<Item> KEY = ITEM.register("key", () -> new KeyItem());
 
     public static final RegistryObject<Item> BLANK_LOCK = ITEM.register(
-            "blank_lock", () -> new Item(new Item.Properties())
+            "blank_lock", () -> new Item(new Item.Properties().group(GROUP))
     );
-
     public static final RegistryObject<Item> LOCK = ITEM.register("lock", () -> new LockItem());
 
     public static final RegistryObject<Item> TEST_LOCK_BLOCK_ITEM = ITEM.register(
             "test_lock_block", () -> new BlockItem(
-                    BDBlocks.TEST_LOCK_BLOCK.get(), new Item.Properties()
+                    BDBlocks.TEST_LOCK_BLOCK.get(), new Item.Properties().group(GROUP)
             )
     );
 
     public static final RegistryObject<Item> KEYSMITH_BLOCK_ITEM = ITEM.register(
             "keysmith_table", () -> new BlockItem(
-                    BDBlocks.KEYSMITH_BLOCK.get(), new Item.Properties()
+                    BDBlocks.KEYSMITH_BLOCK.get(), new Item.Properties().group(GROUP)
             )
     );
     public static final RegistryObject<Item> LOCKSMITH_BLOCK_ITEM = ITEM.register(
             "locksmith_table", () -> new BlockItem(
-                    BDBlocks.LOCKSMITH_BLOCK.get(), new Item.Properties()
+                    BDBlocks.LOCKSMITH_BLOCK.get(), new Item.Properties().group(GROUP)
             )
     );
 }
