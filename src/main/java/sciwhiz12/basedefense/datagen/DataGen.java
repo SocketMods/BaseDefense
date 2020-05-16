70ae601ae28eaf237a54aa6404b87641cbde017d
Arnold Alejo Nunag <arnoldnunag12@gmail.com>
2020-05-16 19:24:57 +0800

    Add data generator
    Currently, only for blockstates (Lockable Door)

---
+++ src/main/java/sciwhiz12/basedefense/datagen/DataGen.java
@@ -0,0 +1,18 @@
+package sciwhiz12.basedefense.datagen;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(bus = Bus.MOD)
+public class DataGen {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        if (event.includeClient()) {
+            generator.addProvider(new BlockStates(generator, event.getExistingFileHelper()));
+        }
+    }
+}
