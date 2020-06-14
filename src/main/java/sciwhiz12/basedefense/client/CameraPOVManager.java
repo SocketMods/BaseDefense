a187f3e1cfb9ae7b98d6dad27353a92d748fb87c
Arnold Alejo Nunag <arnoldnunag12@gmail.com>
2020-06-14 09:36:07 +0800

    Change CameraPOVManager to singleton for debugging

--- src/main/java/sciwhiz12/basedefense/client/CameraPOVManager.java
+++ src/main/java/sciwhiz12/basedefense/client/CameraPOVManager.java
@@ -1,15 +1,20 @@
 package sciwhiz12.basedefense.client;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderNameplateEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
-import net.minecraftforge.event.TickEvent.Phase;
-import net.minecraftforge.event.TickEvent.RenderTickEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.Event.Result;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -17,71 +22,54 @@ import sciwhiz12.basedefense.BaseDefense;
 
 @EventBusSubscriber(value = Dist.CLIENT, bus = Bus.FORGE, modid = BaseDefense.MODID)
 public class CameraPOVManager {
-    public static volatile int cameraEntityId = -1;
-    public static volatile boolean renderingCamera = false;
+    public static CameraPOVManager INSTANCE = null;
 
-    private static Entity prevRenderEntity;
-    private static int currentCameraEntityId;
+    public static void register() {
+        INSTANCE = new CameraPOVManager(Minecraft.getInstance());
+        MinecraftForge.EVENT_BUS.register(INSTANCE);
+    }
 
-    public static void changeEntityID(int newId) {
-        Minecraft mc = Minecraft.getInstance();
-        int newEntityId = newId;
-        if (newId != mc.player.getEntityId() && cameraEntityId != newEntityId) {
-            Entity newEntity = mc.world.getEntityByID(newEntityId);
-            if (newEntityId >= 0 && newEntity != null) {
-                cameraEntityId = newEntityId;
-            } else {
-                cameraEntityId = -1;
-            }
-        } else {
-            cameraEntityId = -1;
-        }
+    public final Minecraft mc;
+    public volatile boolean renderingCamera = false;
+
+    public CameraPOVManager(Minecraft inst) {
+        this.mc = inst;
+    }
+
+    public void changeRenderViewEntity(final int newEntityId) {
+        if (mc.world == null) { return; }
+        Entity newEntity = mc.world.getEntityByID(newEntityId);
+        mc.setRenderViewEntity(newEntity);
+    }
+
+    public boolean isCameraRendering() {
+        // return !(mc.renderViewEntity instanceof PlayerEntity);
+        return true;
     }
 
     @SubscribeEvent
-    static void onRenderTick(RenderTickEvent event) {
-        if (event.phase == Phase.START) {
-            if (cameraEntityId >= 0 && !renderingCamera) {
-                currentCameraEntityId = cameraEntityId;
-                Minecraft mc = Minecraft.getInstance();
-                if (mc == null || mc.world == null) return;
-                Entity newEntity = mc.world.getEntityByID(currentCameraEntityId);
-                if (newEntity != null) {
-                    prevRenderEntity = mc.renderViewEntity;
-                    mc.setRenderViewEntity(newEntity);
-                    renderingCamera = true;
-                } else {
-                    cameraEntityId = -1;
-                }
-            }
-        } else if (event.phase == Phase.END) {
-            if (renderingCamera) {
-                Minecraft mc = Minecraft.getInstance();
-                mc.setRenderViewEntity(prevRenderEntity);
-                prevRenderEntity = null;
-                renderingCamera = false;
-            }
-        }
-    }
-
-    @SubscribeEvent
-    static void onRenderWorldLast(RenderWorldLastEvent event) {
-        if (renderingCamera) {
-            Minecraft mc = Minecraft.getInstance();
-            if (mc == null) return;
+    public void onRenderWorldLast(RenderWorldLastEvent event) {
+        if (isCameraRendering()) {
             EntityRendererManager manager = mc.getRenderManager();
+            ActiveRenderInfo renderInfo = mc.gameRenderer.getActiveRenderInfo();
             float partialTicks = event.getPartialTicks();
-            Entity entity = mc.player;
-            IRenderTypeBuffer buffer = mc.getRenderTypeBuffers().getBufferSource();
-            Vec3d projView = mc.gameRenderer.getActiveRenderInfo().getProjectedView();
-            double x = MathHelper.lerp((double) partialTicks, mc.player.lastTickPosX, mc.player.getPosX());
-            double y = MathHelper.lerp((double) partialTicks, mc.player.lastTickPosY, mc.player.getPosY());
-            double z = MathHelper.lerp((double) partialTicks, mc.player.lastTickPosZ, mc.player.getPosZ());
-            // float yaw = MathHelper.lerp(partialTicks, mc.player.prevRotationYaw,
-            // mc.player.rotationYaw);
-            float yaw = MathHelper.interpolateAngle(partialTicks, mc.player.prevRotationYaw, mc.player.rotationYaw);
-            manager.renderEntityStatic(mc.player, x - projView.x, y - projView.y, z - projView.z, yaw, partialTicks, event
-                .getMatrixStack(), buffer, manager.getPackedLight(entity, partialTicks));
+            ClientPlayerEntity player = mc.player;
+            IRenderTypeBuffer.Impl buffer = mc.getRenderTypeBuffers().getBufferSource();
+            Vec3d projView = renderInfo.getProjectedView();
+            double x = MathHelper.lerp((double) partialTicks, player.lastTickPosX, player.getPosX());
+            double y = MathHelper.lerp((double) partialTicks, player.lastTickPosY, player.getPosY());
+            double z = MathHelper.lerp((double) partialTicks, player.lastTickPosZ, player.getPosZ());
+            float yaw = MathHelper.lerp(partialTicks, mc.player.prevRotationYaw, mc.player.rotationYaw);
+            int packedLight = manager.getPackedLight(player, partialTicks);
+            MatrixStack stack = event.getMatrixStack();
+
+            manager.renderEntityStatic(player, x - projView.x, y - projView.y, z - projView.z, yaw, partialTicks, stack,
+                buffer, packedLight);
         }
+    }
+
+    @SubscribeEvent
+    public void onRenderNameplate(RenderNameplateEvent event) {
+        if (isCameraRendering()) { event.setResult(Result.DENY); }
     }
 }
