0f24a473712d931cda87d023a6433dd64f3e04fb
Arnold Alejo Nunag <arnoldnunag12@gmail.com>
2020-06-14 16:07:53 +0800

    Fix isCameraRendering

--- src/main/java/sciwhiz12/basedefense/client/CameraPOVManager.java
+++ src/main/java/sciwhiz12/basedefense/client/CameraPOVManager.java
@@ -8,6 +8,7 @@ import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.api.distmarker.Dist;
@@ -43,8 +44,7 @@ public class CameraPOVManager {
     }
 
     public boolean isCameraRendering() {
-        // return !(mc.renderViewEntity instanceof PlayerEntity);
-        return true;
+        return !(mc.renderViewEntity instanceof PlayerEntity);
     }
 
     @SubscribeEvent
